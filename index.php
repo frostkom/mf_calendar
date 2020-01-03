a049e28d267210321150f93e9efdfc6253b5dd65
frostkom <martin.fors@frostkom.se>
2020-01-03 06:34:41 +0100

    Stop doing action on delete post

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Calendar
 Plugin URI: https://github.com/frostkom/mf_calendar
 Description: 
-Version: 4.6.2
+Version: 4.6.3
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -52,7 +52,7 @@ if(is_admin())
 	add_action('pre_get_posts', array($obj_calendar, 'pre_get_posts'));
 
 	add_action('wp_trash_post', array($obj_calendar, 'wp_trash_post'));
-	add_action('delete_post', array($obj_calendar, 'wp_trash_post')); // Needs to be here until trash is emptied
+	//add_action('delete_post', array($obj_calendar, 'wp_trash_post')); // Needs to be here until trash is emptied
 }
 
 else
