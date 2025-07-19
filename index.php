622396cdc23356e3cc3603db6581413dbf7def1b
Martin Fors <martin.fors@frostkom.se>
2025-07-19 11:42:51 +0200

    Remove Depends and GitHub URI

--- index.php
+++ index.php
@@ -3,15 +3,12 @@
 Plugin Name: MF Calendar
 Plugin URI: https://github.com/frostkom/mf_calendar
 Description:
-Version: 4.10.3
+Version: 4.10.4
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
 Text Domain: lang_calendar
 Domain Path: /lang
-
-Depends: Meta Box, MF Base
-GitHub Plugin URI: frostkom/mf_calendar
 */
 
 if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') && is_plugin_active("mf_base/index.php"))
