a7a1889f2fefcd2015686562966131270b14a136
frostkom <martin.fors@frostkom.se>
2025-11-03 08:03:30 +0100

    Adjust scroll offset for calendar event focus
    Changed the scroll offset from 100 to 40 pixels when focusing on a calendar event to improve alignment and visibility.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Calendar
 Plugin URI: https://github.com/frostkom/mf_calendar
 Description:
-Version: 4.10.29
+Version: 4.10.30
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
